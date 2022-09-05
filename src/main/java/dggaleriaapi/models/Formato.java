c4eac076ebc18fd4d04d787ed5662227c21755c8
Lucero Raul <luceroraul12@gmail.com>
2022-09-05 13:35:27 -0300

    Initial commit

---
+++ src/main/java/dggaleriaapi/models/Formato.java
@@ -0,0 +1,22 @@
+package dggaleriaapi.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "formatos")
+public class Formato {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String tipo;
+
+}
