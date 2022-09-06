79c0fe3890dfa332c994b48b9c3bcd7292fa90d5
Lucero Raul <luceroraul12@gmail.com>
2022-09-06 18:08:02 -0300

    add respuesta

---
+++ src/main/java/dggaleriaapi/responses/GaleriaResponse.java
@@ -0,0 +1,23 @@
+package dggaleriaapi.responses;
+
+import dggaleriaapi.models.Formato;
+import dggaleriaapi.models.Marca;
+import dggaleriaapi.models.Sabor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+public class GaleriaResponse {
+    private List<Formato>   formatosDiponibles;
+    private List<Marca>     marcasDisponibles;
+    private List<Sabor>     saboresDisponibles;
+
+    private Formato     formatoTrabajado;
+    private Marca       marcasTrabajado;
+    private Sabor       saboresTrabajado;
+}
