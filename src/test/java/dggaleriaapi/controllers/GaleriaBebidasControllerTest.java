fb18427f34ca21c82bc63efed1edd1c7dd5c32dc
Lucero Raul <luceroraul12@gmail.com>
2022-09-06 18:39:29 -0300

    add test inicial

---
+++ src/test/java/dggaleriaapi/controllers/GaleriaBebidasControllerTest.java
@@ -0,0 +1,38 @@
+package dggaleriaapi.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GaleriaBebidasControllerTest {
+
+
+
+    @Test
+    void getAllFormatos() {
+    }
+
+    @Test
+    void saveFormato() {
+    }
+
+    @Test
+    void getAllMarcas() {
+    }
+
+    @Test
+    void saveMarca() {
+    }
+
+    @Test
+    void getAllSaboresByIdMarca() {
+    }
+
+    @Test
+    void getAllSaboresByIdMarcayEnStock() {
+    }
+
+    @Test
+    void saveSaborByIdMarca() {
+    }
+}
