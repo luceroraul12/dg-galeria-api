d9d6406ec2d1791845b6e87e3a2a19d3aa39e6c0
Lucero Raul <luceroraul12@gmail.com>
2022-09-06 18:54:01 -0300

    gen primer test

--- src/test/java/dggaleriaapi/controllers/GaleriaBebidasControllerTest.java
+++ src/test/java/dggaleriaapi/controllers/GaleriaBebidasControllerTest.java
@@ -1,15 +1,33 @@
 package dggaleriaapi.controllers;
 
+import dggaleriaapi.datosprueba.DatosPrueba;
+import dggaleriaapi.repositories.FormatoRepository;
+import dggaleriaapi.responses.GaleriaResponse;
+import dggaleriaapi.services.FormatoService;
+import dggaleriaapi.services.Imp.FormatoServiceImp;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GaleriaBebidasControllerTest {
 
+    static FormatoRepository formatoRepository = Mockito.mock(FormatoRepository.class);
+    FormatoService formatoService = new FormatoServiceImp(formatoRepository);
+    GaleriaBebidasController galeriaBebidasController = new GaleriaBebidasController(formatoService);
 
+    @BeforeAll
+    static void beforeAll() {
+        Mockito.when( formatoRepository.findAll()).thenReturn(DatosPrueba.formatos);
+    }
 
     @Test
     void getAllFormatos() {
+        ResponseEntity<GaleriaResponse> respuesta = galeriaBebidasController.getAllFormatos();
+        assertArrayEquals(DatosPrueba.formatos.toArray(), respuesta.getBody().getFormatosDiponibles().toArray() );
     }
 
     @Test
