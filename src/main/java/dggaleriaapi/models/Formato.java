f9084e18f7e389bdbb10da7079ef9ffa0f648fd5
Lucero Raul <luceroraul12@gmail.com>
2022-09-06 18:43:42 -0300

    add constructor sin args

--- src/main/java/dggaleriaapi/models/Formato.java
+++ src/main/java/dggaleriaapi/models/Formato.java
@@ -1,6 +1,7 @@
 package dggaleriaapi.models;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
@@ -11,6 +12,7 @@ import javax.persistence.Table;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @Table(name = "formatos")
 public class Formato {
     @Id
