1960dfd69b9c81a72ac2a6a8595ddee7131b7106
Lucero Raul <luceroraul12@gmail.com>
2022-09-06 18:21:36 -0300

    desarollo implementaciones

---
+++ src/main/java/dggaleriaapi/services/Imp/FacturaServiceImp.java
@@ -0,0 +1,38 @@
+package dggaleriaapi.services.Imp;
+
+import dggaleriaapi.models.Formato;
+import dggaleriaapi.repositories.FormatoRepository;
+import dggaleriaapi.responses.GaleriaResponse;
+import dggaleriaapi.services.FormatoService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class FacturaServiceImp implements FormatoService {
+
+    @Autowired
+    FormatoRepository formatoRepository;
+
+    public FacturaServiceImp(FormatoRepository formatoRepository) {
+        this.formatoRepository = formatoRepository;
+    }
+
+    @Override
+    public GaleriaResponse getAllFormatos() {
+        GaleriaResponse respuesta = new GaleriaResponse();
+        respuesta.setFormatosDiponibles(
+                formatoRepository.findAll()
+        );
+        return respuesta;
+    }
+
+    @Override
+    public GaleriaResponse saveFormato(String tipo) {
+        Formato formato = new Formato();
+        GaleriaResponse respuesta = new GaleriaResponse();
+
+        formato.setTipo(tipo);
+        respuesta.setFormatoTrabajado(
+                formatoRepository.save(formato)
+        );
+        return respuesta;
+    }
+}
