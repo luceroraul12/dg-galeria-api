f0717ec8c6e3cc67005cdfc7ee61b06f5d3f64e4
Lucero Raul <luceroraul12@gmail.com>
2022-09-07 08:12:44 -0300

    add inyección de dependencia del repo y primera imp

---
+++ src/main/java/dggaleriaapi/services/Imp/SaborServiceImp.java
@@ -0,0 +1,36 @@
+package dggaleriaapi.services.Imp;
+
+import dggaleriaapi.models.Sabor;
+import dggaleriaapi.repositories.SaborRepository;
+import dggaleriaapi.responses.GaleriaResponse;
+import dggaleriaapi.services.SaborService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SaborServiceImp implements SaborService {
+
+    @Autowired
+    SaborRepository saborRepository;
+
+    public SaborServiceImp(SaborRepository saborRepository) {
+        this.saborRepository = saborRepository;
+    }
+
+    @Override
+    public GaleriaResponse getAllByIdMarca(Long idMarca) {
+        GaleriaResponse resultado = new GaleriaResponse();
+        resultado.setSaboresCreados(
+            saborRepository.findByMarca_Id(idMarca);
+        );
+        return resultado;
+    }
+
+    @Override
+    public GaleriaResponse getAllByIdMarcayStock(Long idMarca) {
+        return null;
+    }
+
+    @Override
+    public GaleriaResponse saveSabor(Sabor sabor) {
+        return null;
+    }
+}
