407ba97770deb90cb3dd637804a076112cf6267d
Lucero Raul <luceroraul12@gmail.com>
2022-09-07 14:30:13 -0300

    add update y delete

--- src/main/java/dggaleriaapi/services/FormatoService.java
+++ src/main/java/dggaleriaapi/services/FormatoService.java
@@ -1,5 +1,6 @@
 package dggaleriaapi.services;
 
+import dggaleriaapi.models.Formato;
 import dggaleriaapi.responses.GaleriaResponse;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +9,8 @@ public interface FormatoService {
 
     GaleriaResponse getAllFormatos();
     GaleriaResponse saveFormato(String tipo);
+
+    GaleriaResponse updateFormato(Formato formato);
+
+    GaleriaResponse deleteFormato(Long idFormato);
 }
