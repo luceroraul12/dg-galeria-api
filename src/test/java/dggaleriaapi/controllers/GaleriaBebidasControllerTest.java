5f9a8a8d519160aa351e77aa21c3b9ebacef6dae
Lucero Raul <luceroraul12@gmail.com>
2022-09-07 08:19:18 -0300

    actualizando dependencias del controlador para poder hacer los test con Junit y Mockito

--- src/test/java/dggaleriaapi/controllers/GaleriaBebidasControllerTest.java
+++ src/test/java/dggaleriaapi/controllers/GaleriaBebidasControllerTest.java
@@ -2,9 +2,15 @@ package dggaleriaapi.controllers;
 
 import dggaleriaapi.datosprueba.DatosPrueba;
 import dggaleriaapi.repositories.FormatoRepository;
+import dggaleriaapi.repositories.MarcaRepository;
+import dggaleriaapi.repositories.SaborRepository;
 import dggaleriaapi.responses.GaleriaResponse;
 import dggaleriaapi.services.FormatoService;
 import dggaleriaapi.services.Imp.FormatoServiceImp;
+import dggaleriaapi.services.Imp.MarcaServiceImp;
+import dggaleriaapi.services.Imp.SaborServiceImp;
+import dggaleriaapi.services.MarcaService;
+import dggaleriaapi.services.SaborService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -16,8 +22,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class GaleriaBebidasControllerTest {
 
     static FormatoRepository formatoRepository = Mockito.mock(FormatoRepository.class);
+    static MarcaRepository marcaRepository = Mockito.mock(MarcaRepository.class);
+    static SaborRepository saborRepository = Mockito.mock(SaborRepository.class);
+
     FormatoService formatoService = new FormatoServiceImp(formatoRepository);
-    GaleriaBebidasController galeriaBebidasController = new GaleriaBebidasController(formatoService);
+    MarcaService marcaService = new MarcaServiceImp(marcaRepository);
+    SaborService saborService = new SaborServiceImp(saborRepository);
+
+    GaleriaBebidasController controller = new GaleriaBebidasController(
+            formatoService,
+            marcaService,
+            saborService
+    );
 
     @BeforeAll
     static void beforeAll() {
