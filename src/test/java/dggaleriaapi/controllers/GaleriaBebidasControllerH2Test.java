2fff1f07199d07b94e4c3f20ea30c6bc590fad26
Lucero Raul <luceroraul12@gmail.com>
2022-09-07 14:17:35 -0300

    add tests

--- src/test/java/dggaleriaapi/controllers/GaleriaBebidasControllerH2Test.java
+++ src/test/java/dggaleriaapi/controllers/GaleriaBebidasControllerH2Test.java
@@ -1,9 +1,23 @@
 package dggaleriaapi.controllers;
 
+import dggaleriaapi.responses.GaleriaResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class GaleriaBebidasControllerH2Test {
+
+
+
+    @Autowired
+    GaleriaBebidasController controller;
+
+
 
 
     @BeforeEach
@@ -12,6 +26,8 @@ class GaleriaBebidasControllerH2Test {
 
     @Test
     void getAllFormatos() {
+        ResponseEntity<GaleriaResponse> respuesta = controller.getAllFormatos();
+        assertEquals(5, respuesta.getBody().getFormatosCreados().size());
     }
 
     @Test
@@ -20,6 +36,8 @@ class GaleriaBebidasControllerH2Test {
 
     @Test
     void getAllMarcas() {
+        ResponseEntity<GaleriaResponse> respuesta = controller.getAllMarcas();
+        assertEquals(6, respuesta.getBody().getMarcasCreadas().size());
     }
 
     @Test
@@ -28,10 +46,25 @@ class GaleriaBebidasControllerH2Test {
 
     @Test
     void getAllSaboresByIdMarca() {
+        ResponseEntity<GaleriaResponse> respuesta = controller.getAllSaboresByIdMarca(3L);
+        assertEquals(3, respuesta.getBody().getSaboresCreados().size());
     }
 
     @Test
-    void getAllSaboresByIdMarcayEnStock() {
+    void getSaboresIdStockBien() {
+        ResponseEntity<GaleriaResponse> respuesta = controller.getAllSaboresByIdMarcayEnStock(3L);
+        assertEquals(2, respuesta.getBody().getSaboresDisponibles().size());
+    }
+    @Test
+    void getSaboresIdStockVacio() {
+        ResponseEntity<GaleriaResponse> respuesta = controller.getAllSaboresByIdMarcayEnStock(6L);
+        assertEquals(0, respuesta.getBody().getSaboresDisponibles().size());
+    }
+
+    @Test
+    void getSaboresIdStockNoExiste() {
+        ResponseEntity<GaleriaResponse> respuesta = controller.getAllSaboresByIdMarcayEnStock(203L);
+        assertEquals(0, respuesta.getBody().getSaboresDisponibles().size());
     }
 
     @Test
