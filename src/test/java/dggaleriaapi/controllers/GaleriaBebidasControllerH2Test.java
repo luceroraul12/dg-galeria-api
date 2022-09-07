21f47120149f1a12a72b5e9e386f778f02b0e540
Lucero Raul <luceroraul12@gmail.com>
2022-09-07 11:05:33 -0300

    gen test H2 DB

---
+++ src/test/java/dggaleriaapi/controllers/GaleriaBebidasControllerH2Test.java
@@ -0,0 +1,40 @@
+package dggaleriaapi.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GaleriaBebidasControllerH2Test {
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllFormatos() {
+    }
+
+    @Test
+    void saveFormato() {
+    }
+
+    @Test
+    void getAllMarcas() {
+    }
+
+    @Test
+    void saveMarca() {
+    }
+
+    @Test
+    void getAllSaboresByIdMarca() {
+    }
+
+    @Test
+    void getAllSaboresByIdMarcayEnStock() {
+    }
+
+    @Test
+    void saveSaborByIdMarca() {
+    }
+}
