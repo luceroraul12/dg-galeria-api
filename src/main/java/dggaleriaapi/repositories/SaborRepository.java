612430f119f3d85f7733c0696a10e93e1352b4c1
Lucero Raul <luceroraul12@gmail.com>
2022-09-07 08:08:30 -0300

    add busquedas parametrizadas

--- src/main/java/dggaleriaapi/repositories/SaborRepository.java
+++ src/main/java/dggaleriaapi/repositories/SaborRepository.java
@@ -2,6 +2,14 @@ package dggaleriaapi.repositories;
 
 import dggaleriaapi.models.Sabor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SaborRepository extends JpaRepository<Sabor, Long> {
+    List<Sabor> findByMarca_Id(Long id);
+
+    @Query("select s from Sabor s where s.marca.id = ?1 and s.estadoStock = true")
+    List<Sabor> getAllByIdMacaryStock(Long id);
+
 }
