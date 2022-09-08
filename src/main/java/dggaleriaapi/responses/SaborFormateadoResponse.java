34e75d3a1afcb53d1de1b7c3fb540ff963e47aad
Lucero Raul <luceroraul12@gmail.com>
2022-09-08 20:34:03 -0300

    add response

---
+++ src/main/java/dggaleriaapi/responses/SaborFormateadoResponse.java
@@ -0,0 +1,10 @@
+package dggaleriaapi.responses;
+
+import dggaleriaapi.models.SaborFormateado;
+
+import java.util.List;
+
+public class SaborFormateadoResponse {
+    SaborFormateado saborFormateadoTrabajado;
+    List<SaborFormateado> saboresFormateadosTrabajados;
+}
