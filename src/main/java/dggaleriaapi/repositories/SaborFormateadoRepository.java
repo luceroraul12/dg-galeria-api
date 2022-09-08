4017979d5fd8aaa0df944536a364d31b484ef1b5
Lucero Raul <luceroraul12@gmail.com>
2022-09-08 20:42:24 -0300

    fix metodo

--- src/main/java/dggaleriaapi/repositories/SaborFormateadoRepository.java
+++ src/main/java/dggaleriaapi/repositories/SaborFormateadoRepository.java
@@ -3,5 +3,10 @@ package dggaleriaapi.repositories;
 import dggaleriaapi.models.SaborFormateado;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SaborFormateadoRepository extends JpaRepository<SaborFormateado, Long> {
+    List<SaborFormateado> findBySaborAsociado_Id(Long id);
+
+
 }
