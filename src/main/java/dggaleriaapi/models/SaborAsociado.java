fd6de925bd1ae2c7bcfce0e4028c3addec0de62f
Lucero Raul <luceroraul12@gmail.com>
2022-09-08 15:46:37 -0300

    fix saborAsociado

--- src/main/java/dggaleriaapi/models/SaborAsociado.java
+++ src/main/java/dggaleriaapi/models/SaborAsociado.java
@@ -18,27 +18,14 @@ public class SaborAsociado {
     @Column(name = "sabores_asociados_id_pk", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "nombre")
-    private String nombre;
     @Column(name = "estado_stock")
     private Boolean estadoStock;
-    @ManyToOne
-    @JoinColumn(name = "formatos_id_fk")
-    private Formato formato;
+
     @ManyToOne
     @JoinColumn(name = "marcas_id_fk")
     private Marca marca;
 
-    public SaborAsociado(Long id, String nombre, Marca marca) {
-        this.id = id;
-        this.nombre = nombre;
-        this.marca = marca;
-    }
-
-    public SaborAsociado(Long id, String nombre, Marca marca, Boolean estadoStock) {
-        this.id = id;
-        this.nombre = nombre;
-        this.estadoStock = estadoStock;
-        this.marca = marca;
-    }
+    @ManyToOne
+    @JoinColumn(name = "sabores_id_fk")
+    private Sabor sabor;
 }
