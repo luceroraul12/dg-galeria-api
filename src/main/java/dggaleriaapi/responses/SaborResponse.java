20191a66f87c39642ac5384397bf7952fb9a7798
Lucero Raul <luceroraul12@gmail.com>
2022-09-08 13:31:21 -0300

    add saborResponse

---
+++ src/main/java/dggaleriaapi/responses/SaborResponse.java
@@ -0,0 +1,14 @@
+package dggaleriaapi.responses;
+
+import dggaleriaapi.models.Sabor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class SaborResponse {
+    private List<Sabor> saboresTrabajados;
+    private Sabor saborTrabajado;
+}
