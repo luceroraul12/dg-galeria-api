5d7209c0c9ddc5df30d15851333aec342ac0630c
Lucero Raul <luceroraul12@gmail.com>
2022-09-08 13:31:50 -0300

    add metodos e imp sabor service

--- src/main/java/dggaleriaapi/services/Imp/SaborServiceImp.java
+++ src/main/java/dggaleriaapi/services/Imp/SaborServiceImp.java
@@ -2,10 +2,12 @@ package dggaleriaapi.services.Imp;
 
 import dggaleriaapi.models.Sabor;
 import dggaleriaapi.repositories.SaborRepository;
-import dggaleriaapi.responses.GaleriaResponse;
+import dggaleriaapi.responses.SaborResponse;
 import dggaleriaapi.services.SaborService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SaborServiceImp implements SaborService {
@@ -14,25 +16,51 @@ public class SaborServiceImp implements SaborService {
     SaborRepository saborRepository;
 
     @Override
-    public GaleriaResponse getAll() {
-        GaleriaResponse respuesta = new GaleriaResponse();
-        respuesta.setSabores
-
-        return null;
+    public SaborResponse getAll() {
+        SaborResponse respuesta = new SaborResponse();
+        respuesta.setSaboresTrabajados(
+                saborRepository.findAll()
+        );
+        return respuesta;
     }
 
     @Override
-    public GaleriaResponse save(String nombre) {
-        return null;
+    public SaborResponse save(Sabor sabor) {
+        SaborResponse respuesta = new SaborResponse();
+
+        respuesta.setSaborTrabajado(
+                saborRepository.save(sabor)
+        );
+        return respuesta;
     }
 
     @Override
-    public GaleriaResponse update(Sabor sabor) {
-        return null;
+    public SaborResponse savePorMonton(List<Sabor> sabores) {
+        SaborResponse respuesta = new SaborResponse();
+
+        respuesta.setSaboresTrabajados(
+                saborRepository.saveAll(sabores)
+        );
+        return respuesta;
     }
 
     @Override
-    public GaleriaResponse delete(Sabor sabor) {
-        return null;
+    public SaborResponse update(Sabor sabor) {
+        SaborResponse respuesta = new SaborResponse();
+
+        respuesta.setSaborTrabajado(
+                saborRepository.save(sabor)
+        );
+        return respuesta;
+    }
+
+    @Override
+    public SaborResponse delete(Sabor sabor) {
+        SaborResponse respuesta = new SaborResponse();
+        if (saborRepository.existsById(sabor.getId())){
+            saborRepository.deleteById(sabor.getId());
+            respuesta.setSaborTrabajado(sabor);
+        }
+        return respuesta;
     }
 }
--- src/main/java/dggaleriaapi/services/SaborService.java
+++ src/main/java/dggaleriaapi/services/SaborService.java
@@ -2,13 +2,18 @@ package dggaleriaapi.services;
 
 import dggaleriaapi.models.Sabor;
 import dggaleriaapi.responses.GaleriaResponse;
+import dggaleriaapi.responses.SaborResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface SaborService {
 
-    GaleriaResponse getAll();
-    GaleriaResponse save(String nombre);
-    GaleriaResponse update(Sabor sabor);
-    GaleriaResponse delete(Sabor sabor);
+    SaborResponse getAll();
+    SaborResponse save(Sabor sabor);
+    SaborResponse savePorMonton(List<Sabor> sabores);
+
+    SaborResponse update(Sabor sabor);
+    SaborResponse delete(Sabor sabor);
 }
