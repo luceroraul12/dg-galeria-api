912b15301b89add504c0303d38c2ce8334119231
Lucero Raul <luceroraul12@gmail.com>
2022-09-08 15:46:53 -0300

    delete metodo

--- src/main/java/dggaleriaapi/repositories/SaborAsociadoRepository.java
+++ src/main/java/dggaleriaapi/repositories/SaborAsociadoRepository.java
@@ -9,7 +9,4 @@ import java.util.List;
 public interface SaborAsociadoRepository extends JpaRepository<SaborAsociado, Long> {
     List<SaborAsociado> findByMarca_Id(Long id);
 
-    @Query("select s from SaborAsociado s where s.marca.id = ?1 and s.estadoStock = true")
-    List<SaborAsociado> getAllByIdMacaryStock(Long id);
-
 }
