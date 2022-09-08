fb2b3dbfad39c394d9acd8c0a0ecb3c3261387c3
Lucero Raul <luceroraul12@gmail.com>
2022-09-08 12:39:29 -0300

    add saborformateado entity que se basará en saborasociado y formatos

---
+++ src/main/java/dggaleriaapi/models/SaborFormateado.java
@@ -0,0 +1,25 @@
+package dggaleriaapi.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "sabores_formateados")
+public class SaborFormateado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "sabores_formateados_id_pk", nullable = false)
+    private Long id;
+    @Column(name = "estado_stock")
+    private Boolean estadoStock;
+    @ManyToOne
+    @JoinColumn(name = "formatos_id_fk")
+    private Formato formato;
+    @ManyToOne
+    @JoinColumn(name = "abores_asociados_id_fk")
+    private SaborAsociado saborAsociado;
+}
