cd72936f035da2b5d3c9a06c5ca3c5880dfa5684
Lucero Raul <luceroraul12@gmail.com>
2022-09-07 23:57:51 -0300

    add sabor resumido

--- src/main/java/dggaleriaapi/responses/GaleriaResponse.java
+++ src/main/java/dggaleriaapi/responses/GaleriaResponse.java
@@ -3,6 +3,7 @@ package dggaleriaapi.responses;
 import dggaleriaapi.models.Formato;
 import dggaleriaapi.models.Marca;
 import dggaleriaapi.models.Sabor;
+import dggaleriaapi.models.SaborCliente;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,12 +13,14 @@ import java.util.List;
 @Getter
 @Setter
 public class GaleriaResponse {
-    private List<Formato>   formatosCreados;
-    private List<Marca>     marcasCreadas;
-    private List<Sabor>     saboresDisponibles;
-    private List<Sabor>     saboresCreados;
+    private List<Formato>           formatosCreados;
+    private List<Marca>             marcasCreadas;
+    private List<Sabor>             saboresDisponibles;
+    private List<Sabor>             saboresCreados;
+    private List<SaborCliente>      saboresResumidos;
 
-    private Formato     formatoTrabajado;
-    private Marca       marcaTrabajada;
-    private Sabor       saborTrabajado;
+
+    private Formato                 formatoTrabajado;
+    private Marca                   marcaTrabajada;
+    private Sabor                   saborTrabajado;
 }
