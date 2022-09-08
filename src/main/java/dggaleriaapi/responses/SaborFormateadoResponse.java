2e0cbe6e9e72b41cbb009a7bd463ac9c8c4a976b
Lucero Raul <luceroraul12@gmail.com>
2022-09-08 20:39:20 -0300

    add getter y setter

--- src/main/java/dggaleriaapi/responses/SaborFormateadoResponse.java
+++ src/main/java/dggaleriaapi/responses/SaborFormateadoResponse.java
@@ -1,9 +1,13 @@
 package dggaleriaapi.responses;
 
 import dggaleriaapi.models.SaborFormateado;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
 public class SaborFormateadoResponse {
     SaborFormateado saborFormateadoTrabajado;
     List<SaborFormateado> saboresFormateadosTrabajados;
