e0f8307eb05d1542a9fd0fdcc7076be44552d1d5
Lucero Raul <luceroraul12@gmail.com>
2022-09-08 15:47:27 -0300

    delete metodos asociados

--- src/main/java/dggaleriaapi/services/Imp/SaborAsociadoServiceImp.java
+++ src/main/java/dggaleriaapi/services/Imp/SaborAsociadoServiceImp.java
@@ -1,18 +1,11 @@
 package dggaleriaapi.services.Imp;
 
-import dggaleriaapi.models.Formato;
 import dggaleriaapi.models.SaborAsociado;
-import dggaleriaapi.models.SaborCliente;
 import dggaleriaapi.repositories.SaborAsociadoRepository;
 import dggaleriaapi.responses.GaleriaResponse;
 import dggaleriaapi.services.SaborAsociadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
 
 @Service
 public class SaborAsociadoServiceImp implements SaborAsociadoService {
@@ -29,37 +22,6 @@ public class SaborAsociadoServiceImp implements SaborAsociadoService {
         GaleriaResponse resultado = new GaleriaResponse();
         resultado.setSaboresCreados(
             saborAsociadoRepository.findByMarca_Id(idMarca)
-        );
-        return resultado;
-    }
-
-    @Override
-    public GaleriaResponse getAllByIdMarcaParaVer(Long idMarca) {
-        GaleriaResponse resultado = new GaleriaResponse();
-        List<SaborCliente> saboresResumidos = new ArrayList<>();
-        Set<String> saboresUnicos;
-        List<SaborAsociado> saboresCreados = saborAsociadoRepository.findByMarca_Id(idMarca);
-
-        saboresUnicos = obtenerSaboresUnicos(saboresCreados);
-
-        saboresUnicos.forEach(sabor -> {
-            saboresResumidos.add(
-                    resumirSabor(saboresCreados, sabor)
-            );
-        });
-
-        resultado.setSaboresResumidos(saboresResumidos);
-        return resultado;
-    }
-
-
-
-
-    @Override
-    public GaleriaResponse getAllByIdMarcayStock(Long idMarca) {
-        GaleriaResponse resultado = new GaleriaResponse();
-        resultado.setSaboresDisponibles(
-                saborAsociadoRepository.getAllByIdMacaryStock(idMarca)
         );
         return resultado;
     }
@@ -88,32 +50,6 @@ public class SaborAsociadoServiceImp implements SaborAsociadoService {
         saborAsociadoRepository.delete(saborAsociado);
 
         resultado.setSaborAsociadoTrabajado(saborAsociado);
-        return resultado;
-    }
-
-    private SaborCliente resumirSabor(List<SaborAsociado> saboresCreados, String saborUnico) {
-        List<Boolean> estadosStockCreados = new ArrayList<>();
-        List<Formato> formatosCreados = new ArrayList<>();
-        SaborCliente saborCliente = new SaborCliente();
-
-        saboresCreados.forEach(sabor -> {
-            if (sabor.getNombre().equals(saborUnico)){
-                estadosStockCreados.add(sabor.getEstadoStock());
-                formatosCreados.add(sabor.getFormato());
-            }
-        });
-
-        saborCliente.setNombreSabor(saborUnico);
-        saborCliente.setEstadosStock(estadosStockCreados);
-        saborCliente.setFormatos(formatosCreados);
-        return saborCliente;
-    }
-
-    private Set<String> obtenerSaboresUnicos(List<SaborAsociado> saboresCreados) {
-        Set<String> resultado = new LinkedHashSet<>();
-        saboresCreados.forEach(sabor ->{
-            resultado.add(sabor.getNombre());
-        });
         return resultado;
     }
 }
