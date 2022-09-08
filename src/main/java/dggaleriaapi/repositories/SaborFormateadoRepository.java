c8eb25686e4871290eb5a19f7af96c5b982d0e23
Lucero Raul <luceroraul12@gmail.com>
2022-09-08 12:50:07 -0300

    add repos

---
+++ src/main/java/dggaleriaapi/repositories/SaborFormateadoRepository.java
@@ -0,0 +1,7 @@
+package dggaleriaapi.repositories;
+
+import dggaleriaapi.models.SaborFormateado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaborFormateadoRepository extends JpaRepository<SaborFormateado, Long> {
+}
