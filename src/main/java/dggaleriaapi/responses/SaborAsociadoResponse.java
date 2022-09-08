b442d526cfcf3b81347c92f666e379d71da2454a
Lucero Raul <luceroraul12@gmail.com>
2022-09-08 15:57:11 -0300

    add estructura de momento

--- src/main/java/dggaleriaapi/responses/SaborAsociadoResponse.java
+++ src/main/java/dggaleriaapi/responses/SaborAsociadoResponse.java
@@ -1,12 +1,16 @@
 package dggaleriaapi.responses;
 
+import dggaleriaapi.models.SaborAsociado;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 public class SaborAsociadoResponse {
 
-
+    private List<SaborAsociado> saboresAsociadosTrabajados;
+    private SaborAsociado saborTrabajado;
 
 }
