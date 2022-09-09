f764787b9172b44b5e5fc7c0cf27f981282b9f06
Lucero Raul <luceroraul12@gmail.com>
2022-09-09 00:19:43 -0300

    try catch para saber que paso y que hizo

--- src/main/java/dggaleriaapi/controllers/FormatoController.java
+++ src/main/java/dggaleriaapi/controllers/FormatoController.java
@@ -4,27 +4,81 @@ import dggaleriaapi.models.Formato;
 import dggaleriaapi.responses.FormatoResponse;
 import dggaleriaapi.services.FormatoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/formatos")
 public class FormatoController {
 
     @Autowired
     FormatoService formatoService;
 
-    public FormatoResponse getAllFormatos() {
-        return formatoService.getAllFormatos();
+    @GetMapping
+    public ResponseEntity<FormatoResponse> getAllFormatos() {
+        String mensajeAfirmativo = "formatos obtenidos";
+        String mensajeNegativo = "error al adquirir formatos";
+        FormatoResponse formatoResponse = new FormatoResponse();
+        ResponseEntity<FormatoResponse> respuesta;
+        try {
+            formatoResponse = formatoService.getAllFormatos();
+            formatoResponse.setMensaje(mensajeAfirmativo);
+            respuesta = new ResponseEntity<FormatoResponse>(formatoResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            formatoResponse.setMensaje(mensajeNegativo);
+            respuesta = new ResponseEntity<FormatoResponse>(formatoResponse, HttpStatus.BAD_REQUEST);
+        }
+        return respuesta;
     }
 
-    public FormatoResponse saveFormato(String tipo) {
-        return formatoService.saveFormato(tipo);
+    @PostMapping
+    public ResponseEntity<FormatoResponse> saveFormato(@RequestParam(name = "tipo") String tipo) {
+        String mensajeAfirmativo = "formato guardado";
+        String mensajeNegativo = "error al intentar guardar formato";
+        FormatoResponse formatoResponse = new FormatoResponse();
+        ResponseEntity<FormatoResponse> respuesta;
+        try {
+            formatoResponse = formatoService.saveFormato(tipo);
+            formatoResponse.setMensaje(mensajeAfirmativo);
+            respuesta = new ResponseEntity<FormatoResponse>(formatoResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            formatoResponse.setMensaje(mensajeNegativo);
+            respuesta = new ResponseEntity<FormatoResponse>(formatoResponse, HttpStatus.BAD_REQUEST);
+        }
+        return respuesta;
+    }
+    @PutMapping
+    public ResponseEntity<FormatoResponse> updateFormato(@RequestBody Formato formato) {
+        String mensajeAfirmativo = "formato actualizado";
+        String mensajeNegativo = "error al intentar actualizar formato";
+        FormatoResponse formatoResponse = new FormatoResponse();
+        ResponseEntity<FormatoResponse> respuesta;
+        try {
+            formatoResponse = formatoService.updateFormato(formato);
+            formatoResponse.setMensaje(mensajeAfirmativo);
+            respuesta = new ResponseEntity<FormatoResponse>(formatoResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            formatoResponse.setMensaje(mensajeNegativo);
+            respuesta = new ResponseEntity<FormatoResponse>(formatoResponse, HttpStatus.BAD_REQUEST);
+        }
+        return respuesta;
     }
 
-    public FormatoResponse updateFormato(Formato formato) {
-        return formatoService.updateFormato(formato);
-    }
-
-    public FormatoResponse deleteFormato(Long idFormato) {
-        return formatoService.deleteFormato(idFormato);
+    @DeleteMapping
+    public ResponseEntity<FormatoResponse> deleteFormato(@RequestParam(value = "id-formato") Long idFormato) {
+        String mensajeAfirmativo = "formato eliminado";
+        String mensajeNegativo = "error al intentar eliminar formato";
+        FormatoResponse formatoResponse = new FormatoResponse();
+        ResponseEntity<FormatoResponse> respuesta;
+        try {
+            formatoResponse = formatoService.deleteFormato(idFormato);
+            formatoResponse.setMensaje(mensajeAfirmativo);
+            respuesta = new ResponseEntity<FormatoResponse>(formatoResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            formatoResponse.setMensaje(mensajeNegativo);
+            respuesta = new ResponseEntity<FormatoResponse>(formatoResponse, HttpStatus.BAD_REQUEST);
+        }
+        return respuesta;
     }
 }
