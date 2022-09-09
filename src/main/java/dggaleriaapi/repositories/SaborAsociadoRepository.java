0908d4ba8041e08a6adfc76dc6d34240821d0fe0
Lucero Raul <luceroraul12@gmail.com>
2022-09-09 14:23:54 -0300

    add metodos repo

--- src/main/java/dggaleriaapi/repositories/SaborAsociadoRepository.java
+++ src/main/java/dggaleriaapi/repositories/SaborAsociadoRepository.java
@@ -3,10 +3,27 @@ package dggaleriaapi.repositories;
 import dggaleriaapi.models.SaborAsociado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface SaborAsociadoRepository extends JpaRepository<SaborAsociado, Long> {
     List<SaborAsociado> findByMarca_Id(Long id);
+
+    @Query("select (count(s) > 0) from SaborAsociado s where s.marca.id = :id")
+    boolean existByIdMarca(@Param("id") Long id);
+
+    @Query("select (count(s) > 0) from SaborAsociado s where s.sabor.id = :id")
+    boolean existByIdSabor(@Param("id") Long id);
+
+    @Query("select (count(s) > 0) from SaborAsociado s where s.marca.id = :id and s.sabor.id = :id1")
+    boolean existsByMarca_IdAndSabor_Id(@Param("id") Long idMarca, @Param("id1") Long idSabor);
+
+
+
+
+
+
+
 
 }
