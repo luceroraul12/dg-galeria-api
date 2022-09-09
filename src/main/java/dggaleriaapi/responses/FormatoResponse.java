98c6c935e953ab8144a835a062b850a7e8f3b5cd
Lucero Raul <luceroraul12@gmail.com>
2022-09-08 21:23:43 -0300

    add atributos basicos

--- src/main/java/dggaleriaapi/responses/FormatoResponse.java
+++ src/main/java/dggaleriaapi/responses/FormatoResponse.java
@@ -1,4 +1,14 @@
 package dggaleriaapi.responses;
 
+import dggaleriaapi.models.Formato;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class FormatoResponse {
+    private Formato formatoTrabajado;
+    private List<Formato> formatosTrabajados;
 }
--- src/main/java/dggaleriaapi/responses/MarcaResponse.java
+++ src/main/java/dggaleriaapi/responses/MarcaResponse.java
@@ -1,4 +1,14 @@
 package dggaleriaapi.responses;
 
+import dggaleriaapi.models.Marca;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class MarcaResponse {
+    private Marca marcaTrabajada;
+    private List<Marca> marcasTrabajadas;
 }
