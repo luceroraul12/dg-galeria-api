850c0014884862d29009b02774baec8851f0db87
Lucero Raul <luceroraul12@gmail.com>
2022-09-09 00:48:25 -0300

    try catch para saber que paso y que hizo

--- src/main/java/dggaleriaapi/controllers/MarcaController.java
+++ src/main/java/dggaleriaapi/controllers/MarcaController.java
@@ -4,6 +4,8 @@ import dggaleriaapi.models.Marca;
 import dggaleriaapi.responses.MarcaResponse;
 import dggaleriaapi.services.MarcaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,23 +14,83 @@ public class MarcaController {
     @Autowired
     MarcaService marcaService;
 
-    public MarcaResponse getAll() {
-        return marcaService.getAll();
+    public ResponseEntity<MarcaResponse> getAll() {
+        String mensajeAfirmativo = "marcas obtenidas";
+        String mensajeNegativo = "error al adquirir marcas";
+        MarcaResponse marcaResponse = new MarcaResponse();
+        ResponseEntity<MarcaResponse> respuesta;
+        try {
+            marcaResponse = marcaService.getAll();
+            marcaResponse.setMensaje(mensajeAfirmativo);
+            respuesta = new ResponseEntity<MarcaResponse>(marcaResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            marcaResponse.setMensaje(mensajeNegativo);
+            respuesta = new ResponseEntity<MarcaResponse>(marcaResponse, HttpStatus.BAD_REQUEST);
+        }
+        return respuesta;
     }
 
-    public MarcaResponse getMarcaById(Long idMarca) {
-        return marcaService.getMarcaById(idMarca);
+    public ResponseEntity<MarcaResponse> getMarcaById(Long idMarca) {
+        String mensajeAfirmativo = "marcas obtenidas por id";
+        String mensajeNegativo = "error al adquirir marcas por id";
+        MarcaResponse marcaResponse = new MarcaResponse();
+        ResponseEntity<MarcaResponse> respuesta;
+        try {
+            marcaResponse = marcaService.getMarcaById(idMarca);
+            marcaResponse.setMensaje(mensajeAfirmativo);
+            respuesta = new ResponseEntity<MarcaResponse>(marcaResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            marcaResponse.setMensaje(mensajeNegativo);
+            respuesta = new ResponseEntity<MarcaResponse>(marcaResponse, HttpStatus.BAD_REQUEST);
+        }
+        return respuesta;
     }
 
-    public MarcaResponse saveMarca(Marca marca) {
-        return marcaService.saveMarca(marca);
+    public ResponseEntity<MarcaResponse> saveMarca(Marca marca) {
+        String mensajeAfirmativo = "marca guardada";
+        String mensajeNegativo = "error al guardar marca";
+        MarcaResponse marcaResponse = new MarcaResponse();
+        ResponseEntity<MarcaResponse> respuesta;
+        try {
+            marcaResponse = marcaService.saveMarca(marca);
+            marcaResponse.setMensaje(mensajeAfirmativo);
+            respuesta = new ResponseEntity<MarcaResponse>(marcaResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            marcaResponse.setMensaje(mensajeNegativo);
+            respuesta = new ResponseEntity<MarcaResponse>(marcaResponse, HttpStatus.BAD_REQUEST);
+        }
+        return respuesta;
     }
 
-    public MarcaResponse updateMarca(Marca marca) {
-        return marcaService.updateMarca(marca);
+    public ResponseEntity<MarcaResponse> updateMarca(Marca marca) {
+        String mensajeAfirmativo = "marca actualizada";
+        String mensajeNegativo = "error al actualizar marca";
+        MarcaResponse marcaResponse = new MarcaResponse();
+        ResponseEntity<MarcaResponse> respuesta;
+        try {
+            marcaResponse = marcaService.updateMarca(marca);
+            marcaResponse.setMensaje(mensajeAfirmativo);
+            respuesta = new ResponseEntity<MarcaResponse>(marcaResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            marcaResponse.setMensaje(mensajeNegativo);
+            respuesta = new ResponseEntity<MarcaResponse>(marcaResponse, HttpStatus.BAD_REQUEST);
+        }
+        return respuesta;
     }
 
-    public MarcaResponse deleteMarca(Long idMarca) {
-        return marcaService.deleteMarca(idMarca);
+    public ResponseEntity<MarcaResponse> deleteMarca(Long idMarca) {
+        String mensajeAfirmativo = "marca eliminada";
+        String mensajeNegativo = "error al eliminar marca";
+        MarcaResponse marcaResponse = new MarcaResponse();
+        ResponseEntity<MarcaResponse> respuesta;
+        try {
+            marcaResponse = marcaService.deleteMarca(idMarca);
+            marcaResponse.setMensaje(mensajeAfirmativo);
+            respuesta = new ResponseEntity<MarcaResponse>(marcaResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            marcaResponse.setMensaje(mensajeNegativo);
+            respuesta = new ResponseEntity<MarcaResponse>(marcaResponse, HttpStatus.BAD_REQUEST);
+        }
+        return respuesta;
     }
 }
