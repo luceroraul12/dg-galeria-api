99c7da9776a2286540841a5b2d8fbefabc386467
Lucero Raul <luceroraul12@gmail.com>
2022-09-09 08:20:00 -0300

    add try catch, respons enity

--- src/main/java/dggaleriaapi/controllers/SaborAsociadoController.java
+++ src/main/java/dggaleriaapi/controllers/SaborAsociadoController.java
@@ -4,37 +4,113 @@ import dggaleriaapi.models.SaborAsociado;
 import dggaleriaapi.responses.SaborAsociadoResponse;
 import dggaleriaapi.services.SaborAsociadoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/sabores-asociados")
 public class SaborAsociadoController {
 
     @Autowired
     SaborAsociadoService saborAsociadoService;
 
-    public SaborAsociadoResponse getAll() {
-        return saborAsociadoService.getAll();
+    @GetMapping
+    public ResponseEntity<SaborAsociadoResponse> getAll() {
+        String mensajeAfirmativo = "sabores asociados encontrados";
+        String mensajeNegativo = "error al buscar sabores asociados";
+        SaborAsociadoResponse saborAsociadoResponse = new SaborAsociadoResponse();
+        ResponseEntity<SaborAsociadoResponse> respuesta;
+        try {
+            saborAsociadoResponse = saborAsociadoService.getAll();
+            saborAsociadoResponse.setMensaje(mensajeAfirmativo);
+            respuesta = new ResponseEntity<SaborAsociadoResponse>(saborAsociadoResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            saborAsociadoResponse.setMensaje(mensajeNegativo);
+            respuesta = new ResponseEntity<SaborAsociadoResponse>(saborAsociadoResponse, HttpStatus.BAD_REQUEST);
+        }
+        return respuesta;
     }
-
-    public SaborAsociadoResponse getAllByIdMarca(Long idMarca) {
-        return saborAsociadoService.getAllByIdMarca(idMarca);
+    @GetMapping(value = "/por-marca")
+    public ResponseEntity<SaborAsociadoResponse> getAllByIdMarca(@RequestParam(name = "id") Long idMarca) {
+        String mensajeAfirmativo = "sabores asociados por marca encontrados";
+        String mensajeNegativo = "error al buscar sabores asociados por marca";
+        SaborAsociadoResponse saborAsociadoResponse = new SaborAsociadoResponse();
+        ResponseEntity<SaborAsociadoResponse> respuesta;
+        try {
+            saborAsociadoResponse = saborAsociadoService.getAllByIdMarca(idMarca);
+            saborAsociadoResponse.setMensaje(mensajeAfirmativo);
+            respuesta = new ResponseEntity<SaborAsociadoResponse>(saborAsociadoResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            saborAsociadoResponse.setMensaje(mensajeNegativo);
+            respuesta = new ResponseEntity<SaborAsociadoResponse>(saborAsociadoResponse, HttpStatus.BAD_REQUEST);
+        }
+        return respuesta;
     }
-
-    public SaborAsociadoResponse save(SaborAsociado saborAsociado) {
-        return saborAsociadoService.save(saborAsociado);
+    @PostMapping
+    public ResponseEntity<SaborAsociadoResponse> save(SaborAsociado saborAsociado) {
+        String mensajeAfirmativo = "sabor asociado creado";
+        String mensajeNegativo = "error al crear sabor asociado";
+        SaborAsociadoResponse saborAsociadoResponse = new SaborAsociadoResponse();
+        ResponseEntity<SaborAsociadoResponse> respuesta;
+        try {
+            saborAsociadoResponse = saborAsociadoService.save(saborAsociado);
+            saborAsociadoResponse.setMensaje(mensajeAfirmativo);
+            respuesta = new ResponseEntity<SaborAsociadoResponse>(saborAsociadoResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            saborAsociadoResponse.setMensaje(mensajeNegativo);
+            respuesta = new ResponseEntity<SaborAsociadoResponse>(saborAsociadoResponse, HttpStatus.BAD_REQUEST);
+        }
+        return respuesta;
     }
-
-    public SaborAsociadoResponse savePorMonton(List<SaborAsociado> saboresAsociados) {
-        return saborAsociadoService.savePorMonton(saboresAsociados);
+    @PostMapping(value = "/monton")
+    public ResponseEntity<SaborAsociadoResponse> savePorMonton(List<SaborAsociado> saboresAsociados) {
+        String mensajeAfirmativo = "sabores asociados por marca creados";
+        String mensajeNegativo = "error al crear sabores asociados";
+        SaborAsociadoResponse saborAsociadoResponse = new SaborAsociadoResponse();
+        ResponseEntity<SaborAsociadoResponse> respuesta;
+        try {
+            saborAsociadoResponse = saborAsociadoService.savePorMonton(saboresAsociados);
+            saborAsociadoResponse.setMensaje(mensajeAfirmativo);
+            respuesta = new ResponseEntity<SaborAsociadoResponse>(saborAsociadoResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            saborAsociadoResponse.setMensaje(mensajeNegativo);
+            respuesta = new ResponseEntity<SaborAsociadoResponse>(saborAsociadoResponse, HttpStatus.BAD_REQUEST);
+        }
+        return respuesta;
     }
-
-    public SaborAsociadoResponse update(SaborAsociado saborAsociado) {
-        return saborAsociadoService.update(saborAsociado);
+    @PutMapping
+    public ResponseEntity<SaborAsociadoResponse> update(SaborAsociado saborAsociado) {
+        String mensajeAfirmativo = "sabor asociado actualizado";
+        String mensajeNegativo = "error al actualizar sabor asociado";
+        SaborAsociadoResponse saborAsociadoResponse = new SaborAsociadoResponse();
+        ResponseEntity<SaborAsociadoResponse> respuesta;
+        try {
+            saborAsociadoResponse = saborAsociadoService.update(saborAsociado);
+            saborAsociadoResponse.setMensaje(mensajeAfirmativo);
+            respuesta = new ResponseEntity<SaborAsociadoResponse>(saborAsociadoResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            saborAsociadoResponse.setMensaje(mensajeNegativo);
+            respuesta = new ResponseEntity<SaborAsociadoResponse>(saborAsociadoResponse, HttpStatus.BAD_REQUEST);
+        }
+        return respuesta;
     }
-
-    public SaborAsociadoResponse delete(SaborAsociado saborAsociado) {
-        return saborAsociadoService.delete(saborAsociado);
+    @DeleteMapping
+    public ResponseEntity<SaborAsociadoResponse> delete(SaborAsociado saborAsociado) {
+        String mensajeAfirmativo = "sabor asociado eliminado";
+        String mensajeNegativo = "error al eliminar sabor asociado";
+        SaborAsociadoResponse saborAsociadoResponse = new SaborAsociadoResponse();
+        ResponseEntity<SaborAsociadoResponse> respuesta;
+        try {
+            saborAsociadoResponse = saborAsociadoService.delete(saborAsociado);
+            saborAsociadoResponse.setMensaje(mensajeAfirmativo);
+            respuesta = new ResponseEntity<SaborAsociadoResponse>(saborAsociadoResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            saborAsociadoResponse.setMensaje(mensajeNegativo);
+            respuesta = new ResponseEntity<SaborAsociadoResponse>(saborAsociadoResponse, HttpStatus.BAD_REQUEST);
+        }
+        return respuesta;
     }
 }
