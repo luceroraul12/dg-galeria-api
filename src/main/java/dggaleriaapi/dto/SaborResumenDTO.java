c4c11c13f16b6f95489e9598fb05b769392a2ed5
Lucero Raul <luceroraul12@gmail.com>
2022-09-10 08:59:48 -0300

    gen saborResumenDTO

---
+++ src/main/java/dggaleriaapi/dto/SaborResumenDTO.java
@@ -0,0 +1,10 @@
+package dggaleriaapi.dto;
+
+import dggaleriaapi.models.Formato;
+
+import java.util.List;
+
+public class SaborResumenDTO {
+    private String nombreSabor;
+    private List<Formato> formatosDisponibles;
+}
