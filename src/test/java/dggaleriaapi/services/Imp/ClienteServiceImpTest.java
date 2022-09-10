f9e7bc498b38d01ef359d04c8e48cc527500ed1b
Lucero Raul <luceroraul12@gmail.com>
2022-09-10 11:13:45 -0300

    add test

---
+++ src/test/java/dggaleriaapi/services/Imp/ClienteServiceImpTest.java
@@ -0,0 +1,26 @@
+package dggaleriaapi.services.Imp;
+
+import dggaleriaapi.models.Marca;
+import dggaleriaapi.responses.ClienteDTOResponse;
+import dggaleriaapi.services.ClienteService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ClienteServiceImpTest {
+
+    @Autowired
+    ClienteService service;
+
+    @Test
+    void obtenerResumenPorMarca() {
+        Marca marca = new Marca();
+        marca.setId(3L);
+        ClienteDTOResponse respuesta = service.obtenerResumenPorMarca(marca);
+        System.out.println(respuesta);
+        assertEquals(2, respuesta.getResumen().getSaboresResumidos().get(0).getFormatosDisponibles().size());
+        assertEquals(1, respuesta.getResumen().getSaboresResumidos().get(1).getFormatosDisponibles().size());
+    }
+}
