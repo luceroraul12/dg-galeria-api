a4c45f0ca5d0f1654bae417d1456d870c477d1d4
Lucero Raul <luceroraul12@gmail.com>
2022-09-10 11:11:38 -0300

    add builder para no tener que pelear con los parametros por referencia

--- src/main/java/dggaleriaapi/dto/SaborResumenDTO.java
+++ src/main/java/dggaleriaapi/dto/SaborResumenDTO.java
@@ -1,9 +1,12 @@
 package dggaleriaapi.dto;
 
 import dggaleriaapi.models.Formato;
+import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
-
+@Getter
+@Builder
 public class SaborResumenDTO {
     private String nombreSabor;
     private List<Formato> formatosDisponibles;
