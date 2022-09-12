1863226c09b5f075c186f1e2079a7d613aa3a620
Lucero Raul <luceroraul12@gmail.com>
2022-09-11 22:14:09 -0300

    add getter setter

--- src/main/java/dggaleriaapi/responses/ClienteDTOResponse.java
+++ src/main/java/dggaleriaapi/responses/ClienteDTOResponse.java
@@ -1,7 +1,11 @@
 package dggaleriaapi.responses;
 
 import dggaleriaapi.dto.ClienteDTO;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class ClienteDTOResponse {
     private ClienteDTO resumen;
     private String mensaje;
