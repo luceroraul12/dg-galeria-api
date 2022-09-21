16108ba7b4cfbeb6e1617e820f5c1086a21a26f0
Lucero Raul <luceroraul12@gmail.com>
2022-09-21 10:38:43 -0300

    add exist by name

--- src/main/java/dggaleriaapi/repositories/DrinkContainerRepository.java
+++ src/main/java/dggaleriaapi/repositories/DrinkContainerRepository.java
@@ -2,6 +2,13 @@ package dggaleriaapi.repositories;
 
 import dggaleriaapi.models.DrinkContainer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DrinkContainerRepository extends JpaRepository<DrinkContainer, Long> {
+    @Query("select (count(d) > 0) from DrinkContainer d where upper(d.containerName) = upper(:containerName)")
+    boolean isExistByName(@Param("containerName") String containerName);
+
+
+
 }
