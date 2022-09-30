f1e5cbacf29ef9872c9dfebbbd3b924ca47ebcf8
Lucero Raul <luceroraul12@gmail.com>
2022-09-30 11:42:38 -0300

    fix pdf but i should change url image for static images

--- src/main/java/dggaleriaapi/services/Imp/CustomerPDFServiceImp.java
+++ src/main/java/dggaleriaapi/services/Imp/CustomerPDFServiceImp.java
@@ -4,12 +4,20 @@ package dggaleriaapi.services.Imp;
 import antlr.StringUtils;
 import com.lowagie.text.*;
 import com.lowagie.text.Font;
+import com.lowagie.text.Image;
+import com.lowagie.text.alignment.HorizontalAlignment;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
+import dggaleriaapi.dto.CustomerDTO;
+import dggaleriaapi.dto.TasteResumenDTO;
+import dggaleriaapi.models.Brand;
 import dggaleriaapi.models.BrandedTaste;
+import dggaleriaapi.models.DrinkContainer;
 import dggaleriaapi.models.DrinkContaineredTaste;
+import dggaleriaapi.services.BrandService;
 import dggaleriaapi.services.BrandedTasteService;
+import dggaleriaapi.services.CustomerService;
 import dggaleriaapi.services.DrinkContaineredTasteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,7 +25,10 @@ import org.springframework.stereotype.Service;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.*;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -28,6 +39,12 @@ public class CustomerPDFServiceImp {
 
     @Autowired
     DrinkContaineredTasteService drinkContaineredTasteService;
+
+    @Autowired
+    CustomerService customerService;
+
+    @Autowired
+    BrandService brandService;
 
 
     private void writeTableHeader(PdfPTable table) {
@@ -72,17 +89,17 @@ public class CustomerPDFServiceImp {
     private void sortDrink(List<DrinkContaineredTaste> drinkContaineredTasteList) {
 
 
-        Collections.sort(drinkContaineredTasteList, (d1, d2) -> {
-            return d1.getDrinkContainer().getContainerName() - d2.getDrinkContainer().getContainerName();
+        drinkContaineredTasteList.sort((d1, d2) -> {
+            return 0;
         });
 
-        Collections.sort(drinkContaineredTasteList, (d1, d2) ->{
+        drinkContaineredTasteList.sort((d1, d2) -> {
             return d1.getBrandedTaste().getTaste().getTasteName().compareTo(
-                d2.getBrandedTaste().getTaste().getTasteName()
+                    d2.getBrandedTaste().getTaste().getTasteName()
             );
         });
 
-        Collections.sort(drinkContaineredTasteList, (d1, d2) -> {
+        drinkContaineredTasteList.sort((d1, d2) -> {
             return d1.getBrandedTaste().getBrand().getBrandName().compareTo(
                     d2.getBrandedTaste().getBrand().getBrandName()
             );
@@ -90,7 +107,7 @@ public class CustomerPDFServiceImp {
 
     }
 
-    private String adaptContainerName(Integer ammo){
+    private static String adaptContainerName(Integer ammo){
         String ammoPreFix;
         int ammoFix;
         String result;
@@ -101,7 +118,7 @@ public class CustomerPDFServiceImp {
             ammoPreFix = "ml";
             ammoFix = ammo;
         }
-        result = ammoFix + " " + ammoPreFix;
+        result = ammoFix + ammoPreFix + " ";
         return result;
 
     }
@@ -119,36 +136,113 @@ public class CustomerPDFServiceImp {
         p.setAlignment(Paragraph.ALIGN_CENTER);
 
         document.add(p);
-
         font.setSize(10);
         p = new Paragraph("Fecha de emision: "+ LocalDate.now().toString(),font);
         p.setAlignment(Paragraph.ALIGN_RIGHT);
         document.add(p);
 
-        PdfPTable table = new PdfPTable(3);
-        table.setWidthPercentage(100f);
-        table.setWidths(new float[] {1.5f, 3.5f, 1f});
-        table.setSpacingBefore(10);
 
-        writeTableHeader(table);
-        writeTableData(table);
+        generateTables(document, getAllCustomerDTO());
 
-        document.add(table);
 
         document.close();
 
     }
 
+    private List<CustomerDTO> getAllCustomerDTO() {
+        List<CustomerDTO> result = new ArrayList<>();
+        List<Brand> brands = brandService.getAll().getStockDataResult();
+        brands.forEach(b -> {
+            result.add(
+                    customerService.obtenerResumenPorBrand(b).getCustomerResult()
+            );
+        });
+        return result;
+    }
+
+    private static void generateTables(Document document, List<CustomerDTO> customerList) throws IOException {
+
+        Table imageTable;
+        Table tastesTable;
+        Cell brandCell;
+        Cell iconicBrandCell;
+        Cell contenedorSaboresCell;
+        Cell tamanioCell;
+
+
+        for (CustomerDTO customer : customerList){
+
+            Brand brand = customer.getBrandSelected();
+            List<TasteResumenDTO> tasteResumenDTOList = customer.getTasteResults();
+
+            Image brandImage = Image.getInstance(new URL(brand.getUrl()));
+            brandImage.scaleAbsolute(150,150);
+
+            imageTable = new Table(2);
+            imageTable.setBorderWidth(1);
+            imageTable.setPadding(5);
+            imageTable.setSpacing(5);
+            imageTable.setWidth(100);
+
+            tastesTable = new Table(2);
+            tastesTable.setBorderWidth(1);
+            tastesTable.setPadding(5);
+            tastesTable.setSpacing(5);
+            tastesTable.setWidth(100);
+
+            brandCell = new Cell();
+            brandCell.setHorizontalAlignment(HorizontalAlignment.CENTER);
+            brandCell.add(brandImage);
+            brandCell.add(new Paragraph("MARCA "));
+            imageTable.addCell(brandCell,0,0);
+
+            iconicBrandCell = new Cell();
+            iconicBrandCell.setHorizontalAlignment(HorizontalAlignment.CENTER);
+            iconicBrandCell.add(brandImage);
+            iconicBrandCell.add(new Chunk("PRODUCTO ICONICO "));
+            imageTable.addCell(iconicBrandCell,0,1);
+
+            contenedorSaboresCell = new Cell();
+            contenedorSaboresCell.setColspan(2);
+            contenedorSaboresCell.setRowspan(2);
+
+            Font font = new Font();
+            font.setColor(Color.BLUE);
+            font.setSize(15);
+            Paragraph p = new Paragraph(brand.getBrandName().toUpperCase(),font);
+            p.setAlignment(Element.ALIGN_CENTER);
+            p.setSpacingBefore(10);
+
+            tastesTable.addCell("Sabores");
+            tastesTable.addCell("Tamaños");
+
+            for (TasteResumenDTO taste : tasteResumenDTOList){
+
+                List<DrinkContainer> containers = taste.getDrinkContainersAvailable();
+
+                tastesTable.addCell(taste.getTasteName().toUpperCase());
+                tamanioCell =  new Cell();
+
+                for (DrinkContainer dc : containers){
+                    tamanioCell.add(new Chunk(
+                            adaptContainerName(dc.getContainerName())
+                    ));
+                }
+                tastesTable.addCell(tamanioCell);
+
+                contenedorSaboresCell.add(tastesTable);
+
+            }
+
+            document.add(p);
+            document.add(imageTable);
+            document.add(tastesTable);
+
+        }
 
 
 
-
-
-
-
-
-
-
+    }
 
 
 }
